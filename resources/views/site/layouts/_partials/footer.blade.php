4157a150b280b82867fa7c9f231c4b6fb80c718c
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-09-21 11:15:04 -0300

    refatore: modificated estruture, remove codes relacionship with views... routes or resources

--- resources/views/site/layouts/_partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="text-muted text-center bg-primary">
-    <div class="container">
-        <p class="text-white py-4">© 2023 Biblioteca Pedbot</p>
-    </div>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\PublisherController;
-use App\Http\Controllers\RentalController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,38 +14,4 @@ use App\Http\Controllers\RentalController;
 */
 
 Route::middleware('autenticate:padrao')->prefix('/app')->group(function () {
-    // Define routes for the 'author' resource
-    Route::resource('/author', AuthorController::class);
-
-    // Define routes for the 'book' resource
-    Route::resource('/book', BookController::class);
-
-    // Define routes for the 'customer' resource
-    Route::resource('/customer', CustomerController::class);
-
-    // Define routes for the 'employee' resource
-    Route::resource('/employee', EmployeeController::class);
-
-    // Define routes for the 'publisher' resource
-    Route::resource('/publisher', PublisherController::class);
-
-    // Define routes for the 'rental' resource
-    Route::resource('/rental', RentalController::class);
-    
-    // Define a route for the 'menu' endpoint
-    Route::get('/menu', [\App\Http\Controllers\MenuController::class, 'index'])->name('site.menu');
-
-    // Define a route for the 'logout' endpoint
-    Route::get('/logout', [\App\Http\Controllers\LoginController::class, 'logout'])->name('site.logout');
-    
-    // Define a route for the 'home' endpoint
-    Route::get('/home', [\App\Http\Controllers\HomePageController::class, 'index'])->name('site.home');
 });
-
-
-Route::get('/login/{erro?},{sucess?}', [\App\Http\Controllers\LoginController::class, 'index'])->name('site.login');
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'authenticate'])->name('site.login');
-
-
-Route::get('/register', [\App\Http\Controllers\RegisterController::class, 'index'])->name('site.register');
-Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'create'])->name('site.register');
